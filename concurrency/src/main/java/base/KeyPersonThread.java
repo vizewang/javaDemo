17779ed3da79d094b7462c220d1a84c3bec907bf
vizewang <vizewang@163.com>
2015-07-21 23:14:22 +0800

    add

---
+++ concurrency/src/main/java/base/KeyPersonThread.java
@@ -0,0 +1,15 @@
+package base;
+
+/**
+ * Created by vizewang on 2015/7/21.
+ */
+public class KeyPersonThread extends Thread {
+    public void run(){
+        System.out.println(Thread.currentThread().getName()+"开始了战斗");
+        for(int i=0;i<10;i++){
+            System.out.println(Thread.currentThread().getName()+"左突右杀，攻击隋军..");
+        }
+        System.out.println(Thread.currentThread().getName()+"结束了战斗");
+
+    }
+}
--- concurrency/src/main/java/base/Stage.java
+++ concurrency/src/main/java/base/Stage.java
@@ -5,6 +5,20 @@ package base;
  */
 public class Stage extends Thread {
     public void run() {
+        System.out.println("欢迎观看隋唐演义");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("徐徐拉开大幕");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("话说隋朝末年");
+
         ArmyRunnable armyTaskOfSuiDynasty = new ArmyRunnable();
         ArmyRunnable armyTaskOfRevolt = new ArmyRunnable();
         Thread armyOfSuiDynasty = new Thread(armyTaskOfSuiDynasty, "隋军");
@@ -18,13 +32,28 @@ public class Stage extends Thread {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        System.out.println("正当双方激战正酣，半路杀出个程咬金");
+        Thread mrCheng=new KeyPersonThread();
+        mrCheng.setName("程咬金");
+        System.out.println("程咬金的理想就是结束战争，使百姓安居乐业");
+
         armyTaskOfSuiDynasty.setKeepRunning(false);
         armyTaskOfRevolt.setKeepRunning(false);
+
+
         try {
-            armyOfRevolt.join();
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        mrCheng.start();
+        try {
+            mrCheng.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("战争结束，人民安居乐业，");
     }
 
     public static void main(String[] args) {
