45ca17629cffe6c981d1d737ca6aa1cb96148fab
weizwang <weizwang@ebay.com>
2015-09-01 09:18:23 +0800

    add

---
+++ tijDemo/src/main/java/strings/First.java
@@ -0,0 +1,11 @@
+package strings;
+
+/**
+ * Created by weizwang on 2015/9/1.
+ */
+public class First {
+    public static void main(String[] args) {
+        String t="test";
+        System.out.println(t);
+    }
+}
