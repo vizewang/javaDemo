61fe620a54f64071f0390ee5b22256a7b86ef9a5
weizwang <weizwang@ebay.com>
2015-11-20 09:17:32 +0800

    add demo

---
+++ tijDemo/src/main/java/containers/FailFast.java
@@ -0,0 +1,23 @@
+package containers;//: containers/FailFast.java
+// Demonstrates the "fail-fast" behavior.
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+public class FailFast {
+  public static void main(String[] args) {
+    Collection<String> c = new ArrayList<String>();
+    Iterator<String> it = c.iterator();
+    c.add("An object");
+    try {
+      String s = it.next();
+      System.out.println(s);
+    } catch(ConcurrentModificationException e) {
+      System.out.println(e);
+    }
+  }
+} /* Output:
+java.util.ConcurrentModificationException
+*///:~
