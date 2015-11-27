9d8f2087a09cd232a402fbc9b0ab51c8c3156382
weizwang <weizwang@ebay.com>
2015-11-27 08:28:23 +0800

    add demo

---
+++ tijDemo/src/main/java/io/MemoryInput.java
@@ -0,0 +1,15 @@
+package io;//: io/MemoryInput.java
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public class MemoryInput {
+  public static void main(String[] args)
+  throws IOException {
+    StringReader in = new StringReader(
+      BufferedInputFile.read("/test.json"));
+    int c;
+    while((c = in.read()) != -1)
+      System.out.print((char)c);
+  }
+} /* (Execute to see output) *///:~
